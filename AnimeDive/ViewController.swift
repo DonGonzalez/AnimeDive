560725d25207ac15d788b27c6567ffb5e6e4c417
DonGonzalez <dongonzales818@gmail.com>
2022-06-25 19:52:55 +0200

    Initial Commit

---
+++ AnimeDive/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  AnimeDive
+//
+//  Created by Robert B on 25/06/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+// grafic shell + button action
+    
+    
+    var viewModel: MyViewModel?
+    // funkcja, ktora uruchamiasz po stworzenie instancji view controllera
+    func assignDependencies(viewModel: MyViewModel) {
+        self.viewModel = viewModel
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+       
+    }
+
+
+}
+
